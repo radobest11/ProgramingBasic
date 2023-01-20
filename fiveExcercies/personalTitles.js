24c7322ecd3e480404811de4912aad32a998a59d
radobest11 <radoslav.stanchev.1989@gmail.com>
2023-01-20 20:46:13 +0200

    fifthExercies

---
+++ fiveExcercies/personalTitles.js
@@ -0,0 +1,38 @@
+function personalTitles (input) {
+
+    let age = Number(input[0]);
+    let sex = input[1];
+
+    if (sex == 'm') {
+
+        if (age < 16) {
+
+            console.log("Master");
+
+        } else {
+
+            console.log("Mr.")
+        }
+    }
+
+    else if ( sex == 'f') {
+
+
+        if (age < 16) {
+
+            console.log("Miss");
+
+        } else {
+
+            console.log("Ms.");
+        }
+    }
+
+}
+
+personalTitles(["16",
+"m"])
+
+
+
+
