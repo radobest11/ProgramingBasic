b90719c55136fa8482497c264d85c2d7a79a6fe3
radobest11 <radoslav.stanchev.1989@gmail.com>
2023-02-03 19:45:57 +0200

    sevenExercies

---
+++ sevenExercies/numbersnTo1.js
@@ -0,0 +1,11 @@
+function numbersnTo1 (input) {
+
+    let num = Number(input[0]);
+
+    for (let i = num; i > 0; i--) {
+
+        console.log(i);
+    }
+
+}
+numbersnTo1(["5"])
---
+++ sevenExercies/sumOfNumbers.js
@@ -0,0 +1,16 @@
+function sumOfNumber (input) {
+
+    let num = input[0];
+
+    let sum = 0;
+
+
+    for (let i = 0; i < num.length; i++) {
+
+        sum += Number(num[i]);
+
+    }
+
+    console.log(`The sum of the digits is:${sum}`)
+}
+sumOfNumber([156])
